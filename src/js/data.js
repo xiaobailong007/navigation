be1a9543c06fa4c69eda3bdc700e0ce664e16a8c
xiaobailong007 <15820197555@163.com>
2020-12-07 09:14:59 +0800

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -1359,13 +1359,13 @@ var boxList = new Vue({
           {
             href: 'http://45.133.119.107:43990/',
             src: require('../images/宝塔面板.ico').default,
-            name: '服务器-小白龙次',
+            name: '服务器-小白龙副',
             text: '宝塔管理界面'
           },
           {
             href: 'http://152.32.240.226:43990/long99',
             src: require('../images/宝塔面板.ico').default,
-            name: '服务器-小白龙主1',
+            name: '服务器-小白龙主',
             text: '宝塔管理界面'
           },
           {
